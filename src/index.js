8cbcf77cfbc8fd47c3c226c0fc1cad4a702d9bb7
Akash Shaw <aakashshaw88@gmail.com>
2020-02-03 22:15:49 +0530

    Adding Project

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// --------------Index.js---------------------
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position), 
+//         (err) => console.log(err) 
+//     );
+//     return <div></div>;
+// };
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            lat: null,
+            errorMessage: ''
+        };      
+    }
+    componentDidMount(){
+         window.navigator.geolocation.getCurrentPosition(
+            position =>  this.setState({lat : position.coords.latitude}), 
+             err => this.setState({errorMessage: err.message})      
+    );
+}
+    // componentDidUpdate(){
+    //     console.log('My component was just updated - It rerendered');
+    // }
+    renderContent(){
+            if (this.state.errorMessage && !this.state.lat ) {
+                return <div>Error: {this.state.errorMessage} </div>
+            }
+            if (!this.state.errorMessage && this.state.lat) {
+                return <SeasonDisplay lat={this.state.lat} />  
+            }
+            return <div><Spinner message="Please accept location request"/></div>;
+  }
+    render(){
+            return <div className="border red">
+            {this.renderContent()}
+        </div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
